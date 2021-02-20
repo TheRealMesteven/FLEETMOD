f4af7da44aa7b69d6afff5e057c3aeb82a64250c
Steven <mestevenyoutubing@gmail.com>
2021-02-20 15:28:26 +0000

    Fixed, removed the testing anyone can F1 and create a new ship & removed timer for spawning ships

--- FLEETMOD/UpdatePLTabMenu.cs
+++ FLEETMOD/UpdatePLTabMenu.cs
@@ -15,17 +15,17 @@ namespace FLEETMOD
 			bool isrunningmod = MyVariables.isrunningmod;
 			if (isrunningmod)
 			{
-				bool flag = __instance != null && PLEncounterManager.Instance.PlayerShip != null && PLServer.Instance != null && PLNetworkManager.Instance.LocalPlayer != null && PLServer.Instance.GameHasStarted && PLNetworkManager.Instance.LocalPlayer.GetHasStarted() && PLNetworkManager.Instance.LocalPlayer.PlayerLifeTime > 3f;
+                bool flag = __instance != null && PLEncounterManager.Instance.PlayerShip != null && PLServer.Instance != null && PLNetworkManager.Instance.LocalPlayer != null && PLServer.Instance.GameHasStarted && PLNetworkManager.Instance.LocalPlayer.GetHasStarted() && PLNetworkManager.Instance.LocalPlayer.PlayerLifeTime > 3f;
 				if (flag)
 				{
-					List<PLShipInfoBase> list = new List<PLShipInfoBase>();
+                    List<PLShipInfoBase> list = new List<PLShipInfoBase>();
 					foreach (PLShipInfoBase plshipInfoBase in PLEncounterManager.Instance.AllShips.Values)
 					{
 						bool flag2 = plshipInfoBase.TagID == -23 && plshipInfoBase != null;
 						if (flag2)
 						{
 							list.Add(plshipInfoBase);
-						}
+                        }
 					}
 					bool flag3 = !PLNetworkManager.Instance.IsTyping && Input.GetKeyDown(KeyCode.Comma) && PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp > 0 && __instance.TabMenuActive;
 					if (flag3)
@@ -33,24 +33,24 @@ namespace FLEETMOD
 						PLMusic.PostEvent("play_titlemenu_ui_click", __instance.gameObject);
 						PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp = PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp - 1;
 					}
-					bool flag4 = !PLNetworkManager.Instance.IsTyping && Input.GetKeyDown(KeyCode.Period) && PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp < list.Count - 1 && __instance.TabMenuActive;
+                    bool flag4 = !PLNetworkManager.Instance.IsTyping && Input.GetKeyDown(KeyCode.Period) && PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp < list.Count - 1 && __instance.TabMenuActive;
 					if (flag4)
 					{
 						PLMusic.PostEvent("play_titlemenu_ui_click", __instance.gameObject);
 						PLPlayer localPlayer = PLNetworkManager.Instance.LocalPlayer;
 						localPlayer.FBBiscuitsSoldSinceWarp++;
 					}
-					List<PLPlayer> list2 = new List<PLPlayer>();
-					for (int i = 0; i < PLServer.Instance.AllPlayers.Count; i++)
+                    List<PLPlayer> list2 = new List<PLPlayer>();;
+                    for (int i = 0; i < PLServer.Instance.AllPlayers.Count; i++)
 					{
-						PLPlayer plplayer = PLServer.Instance.AllPlayers[i];
-						bool flag5 = plplayer != null && plplayer.TeamID == 0 && plplayer.StartingShip != null && plplayer.StartingShip == list[PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp];
-						if (flag5)
+                        PLPlayer plplayer = PLServer.Instance.AllPlayers[i];
+                        bool flag5 = plplayer != null && plplayer.TeamID == 0 && plplayer.StartingShip != null && plplayer.StartingShip == list[PLNetworkManager.Instance.LocalPlayer.FBBiscuitsSoldSinceWarp];
+                        if (flag5)
 						{
-							list2.Add(plplayer);
-						}
+                            list2.Add(plplayer);
+                        }
 					}
-					list2.Sort(delegate(PLPlayer p1, PLPlayer p2)
+                    list2.Sort(delegate(PLPlayer p1, PLPlayer p2)
 					{
 						int num2 = p1.GetClassID();
 						int num3 = p2.GetClassID();
@@ -66,7 +66,7 @@ namespace FLEETMOD
 						}
 						return num2.CompareTo(num3);
 					});
-					int num = 0;
+                    int num = 0;
 					for (int j = 0; j < __instance.PlayerInfoDisplays.Length; j++)
 					{
 						PLOverviewPlayerInfoDisplay ploverviewPlayerInfoDisplay = __instance.PlayerInfoDisplays[j];
@@ -88,7 +88,7 @@ namespace FLEETMOD
 						ploverviewPlayerInfoDisplay.MyPlayer = plplayer2;
 						num++;
 					}
-				}
+                }
 			}
 		}
 	}
