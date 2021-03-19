760e458c16ff3908b2195b32dcc2e3da41ee17e6
DragonFire47 <1redyoung@gmail.com>
2021-03-19 15:18:59 -0400

    -Added Fleet ID Syncing

--- FLEETMOD/Update.cs
+++ FLEETMOD/Update.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using ExitGames.Client.Photon;
+﻿using System.Collections.Generic;
 using HarmonyLib;
-using Steamworks;
 using UnityEngine;
 
 namespace FLEETMOD
@@ -9,6 +7,7 @@ namespace FLEETMOD
     [HarmonyPatch(typeof(PLServer), "Update")]
     internal class Update
     {
+        static Dictionary<int, int> CachedFleet = Global.Fleet;
         public static void Postfix(PLServer __instance)
         {
             if (__instance != null && __instance.GameHasStarted && PLNetworkManager.Instance.LocalPlayer != null && PLNetworkManager.Instance.LocalPlayer.GetHasStarted() && PLEncounterManager.Instance.PlayerShip != null)
@@ -23,6 +22,35 @@ namespace FLEETMOD
                         PLTabMenu.Instance.TabMenuActive = false;
                     }
                 }
+            }
+            if(PhotonNetwork.isMasterClient) //MasterClient Check for all masterclient code
+            {
+                if (CachedFleet != Global.Fleet)
+                {
+                    CachedFleet = Global.Fleet;
+                    foreach (PLPlayer player in PLServer.Instance.AllPlayers)
+                    { //for every player in PLServer.AllPlayers, check they are real and not the local player before sending ModMessage containing current Fleet.
+                        PhotonPlayer photonplayer = player.GetPhotonPlayer();
+                        if (!player.IsBot && photonplayer != PhotonNetwork.player)
+                        {
+                            PulsarPluginLoader.ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.SyncFleetIDs", photonplayer, new object[] { Global.Fleet });
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PLServer), "LoginMessage")] //Initial Sync. If multiple messages exist consider making new Modmessage for initial sync
+    class FleetSyncPatches
+    {
+        static void Postfix(PhotonPlayer newPhotonPlayer)
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                PulsarPluginLoader.ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.SyncFleetIDs", newPhotonPlayer, new object[] {
+                    Global.Fleet
+                });
             }
         }
     }
