cf11e889caa832a3d4d8f5e23b6f63c945c78559
Steven <mestevenyoutubing@gmail.com>
2021-03-19 18:34:13 +0000

    Cleared out Started working on Update & such

---
+++ FLEETMOD/_Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLEETMOD
+{
+    internal class _Variables
+    {
+        public static bool isrunningmod;
+        // Boolean check if the mod is running
+    }
+}
