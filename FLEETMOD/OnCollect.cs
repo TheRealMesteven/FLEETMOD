87cb28a0c8a9497cd282857e1aff026b83522b25
Steven <mestevenyoutubing@gmail.com>
2021-12-23 20:31:19 +0000

    Added the Rewrites sensor-dish and default scrap pickup

--- FLEETMOD/OnCollect.cs
+++ FLEETMOD/OnCollect.cs
@@ -1,77 +1,147 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
 using HarmonyLib;
+using PulsarModLoader;
 using UnityEngine;
+using static HarmonyLib.AccessTools;
+using static PulsarModLoader.Utilities.Logger;
+using static PulsarModLoader.Patches.HarmonyHelpers;
+using static System.Reflection.Emit.OpCodes;
 
 namespace FLEETMOD
 {
-	[HarmonyPatch(typeof(PLSpaceScrap), "OnCollect")]
-	internal class OnCollect
-	{
-		public static bool Prefix(PLSpaceScrap __instance, ref int ___m_EncounterNetID)
-		{
-			if (!MyVariables.isrunningmod)
-			{
-				return true;
-			}
-			else
-			{
-				if (!__instance.Collected)
-				{
-					PLShipInfo plshipInfo = PLEncounterManager.Instance.GetShipFromID(PLNetworkManager.Instance.LocalPlayer.FBSellAttemptsLeft) as PLShipInfo;
-					if (PLAcademyShipInfo.Instance != null)
-					{
-						plshipInfo = PLAcademyShipInfo.Instance;
-					}
-					if (PhotonNetwork.isMasterClient && plshipInfo != null && PLServer.Instance != null)
-					{
-						PLSlot slot = plshipInfo.MyStats.GetSlot(ESlotType.E_COMP_CARGO);
-						if (slot != null && (slot.Count < slot.MaxItems || plshipInfo.ShipTypeID == EShipType.E_ACADEMY))
-						{
-							__instance.Collected = true;
-							PLServer.Instance.photonView.RPC("ScrapCollectedEffect", PhotonTargets.All, new object[]
-							{
-								__instance.transform.position
-							});
-							if (plshipInfo.ShipTypeID == EShipType.E_ACADEMY)
-							{
-								return false;
-							}
-							PLServer.Instance.photonView.RPC("ScrapLateCollected", PhotonTargets.All, new object[]
-							{
-								__instance.EncounterNetID
-							});
-							if (__instance.IsSpecificComponentScrap)
-							{
-								plshipInfo.MyStats.AddShipComponent(PLWare.CreateFromHash(1, __instance.SpecificComponent_CompHash) as PLShipComponent, -1, ESlotType.E_COMP_CARGO);
-								return false;
-							}
-							if (__instance.CanGiveComponent)
-							{
-								PLRand plrand = new PLRand(PLServer.Instance.GalaxySeed + PLServer.Instance.GetCurrentHubID() + ___m_EncounterNetID);
-								int num = plrand.Next(0, 200);
-								if (PLEncounterManager.Instance.PlayerShip.ShipTypeID == EShipType.E_CARRIER)
-								{
-									num = plrand.Next(0, 75);
-								}
-								Mathf.RoundToInt(Mathf.Pow(plrand.Next(0f, 1f), 4f) * PLServer.Instance.ChaosLevel);
-								PLShipComponent plshipComponent = null;
-								if (num < 50 && __instance.SpecificComponent_CompHash != -1)
-								{
-									plshipComponent = PLShipComponent.CreateShipComponentFromHash(__instance.SpecificComponent_CompHash, null);
-								}
-								if (plshipComponent == null)
-								{
-									plshipComponent = new PLScrapCargo(0);
-								}
-								plshipInfo.MyStats.AddShipComponent(plshipComponent, -1, ESlotType.E_COMP_CARGO);
-								return false;
-							}
-							plshipInfo.MyStats.AddShipComponent(new PLScrapCargo(0), -1, ESlotType.E_COMP_CARGO);
-						}
-					}
-				}
-				return false;
-			}
-		}
-	}
+    [HarmonyPatch(typeof(PLSpaceScrap), "OnCollect")]
+    class SpaceSrapOnCollect
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> target = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldsfld, Field(typeof(PLEncounterManager), "Instance")),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PLLevelSync), "PlayerShip"))
+            }; // PLShipInfo plshipInfo = PLEncounterManager.Instance.PlayerShip;
+
+        List<CodeInstruction> patch = new List<CodeInstruction>()
+            {
+                new CodeInstruction(OpCodes.Ldarg_0), // this
+                new CodeInstruction(OpCodes.Call, Method(typeof(PlayerShipReplace), "PatchShip"))
+            }; // result: PLShipInfo plshipInfo = FLEETMOD.PlayerShipReplace.PatchShip(this);
+
+            return PatchBySequence(instructions, target, patch, PatchMode.REPLACE, showDebugOutput: false);
+    }
+}
+
+[HarmonyPatch(typeof(PLSpaceScrap), "Update")]
+class SpaceScrapUpdate
+{
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        return PatchBySequence(instructions,
+            new CodeInstruction[]
+        {
+                new CodeInstruction(OpCodes.Ldsfld, Field(typeof(PLEncounterManager), "Instance")),
+                new CodeInstruction(OpCodes.Ldfld, Field(typeof(PLLevelSync), "PlayerShip")),
+                new CodeInstruction(OpCodes.Stloc_0)
+            }, 
+                new CodeInstruction[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_0), // this
+                new CodeInstruction(OpCodes.Call, Method(typeof(PlayerShipReplace), "PatchShip")),
+                new CodeInstruction(OpCodes.Stloc_0)
+}, PatchMode.AFTER, CheckMode.ALWAYS, false);
+            /*
+             * Before: PLShipInfo plshipInfo = PLEncounterManager.Instance.PlayerShip;
+             * After: PLShipInfo plshipInfo = PLEncounterManager.Instance.PlayerShip;
+             *       plshipInfo = FLEETMOD.PlayerShipReplace.PatchShip(this);
+             */
+        }
+    }
+
+    [HarmonyPatch(typeof(PLShipInfo), "UpdateSensorDish")]
+class UpdateSensorDishPatch
+{
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        return PatchBySequence(instructions,
+            new CodeInstruction[]
+            {
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Call, Method(typeof(Photon.MonoBehaviour), "get_photonView")),
+                    new CodeInstruction(OpCodes.Ldstr, "RequestScrapCollectFromSensorDish"),
+                    new CodeInstruction(OpCodes.Ldc_I4_2),
+                    new CodeInstruction(OpCodes.Ldc_I4_1),
+                    new CodeInstruction(OpCodes.Newarr, typeof(System.Object)),
+                    new CodeInstruction(OpCodes.Dup),
+                    new CodeInstruction(OpCodes.Ldc_I4_0),
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Ldfld, Field(typeof(PLShipInfo), "SensorDishCurrentSecondaryTarget_Scrap")),
+                    new CodeInstruction(OpCodes.Callvirt, Method(typeof(PLSpecialEncounterNetObject), "get_EncounterNetID")),
+                    new CodeInstruction(OpCodes.Box, typeof(int)),
+                    new CodeInstruction(OpCodes.Stelem_Ref),
+                    new CodeInstruction(OpCodes.Callvirt, Method(typeof(PhotonView), "RPC", new[] { typeof(string), typeof(PhotonTargets), typeof(object[]) }))
+                    /*
+                     base.photonView.RPC("RequestScrapCollectFromSensorDish", PhotonTargets.MasterClient, new object[]
+				    {
+					    this.SensorDishCurrentSecondaryTarget_Scrap.EncounterNetID
+				    });
+                     */
+                },
+                new CodeInstruction[]
+                {
+                    new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Ldfld, Field(typeof(PLShipInfo), "SensorDishCurrentSecondaryTarget_Scrap")),
+                    new CodeInstruction(OpCodes.Callvirt, Method(typeof(PLSpecialEncounterNetObject), "get_EncounterNetID")),
+                    new CodeInstruction(OpCodes.Call, Method(typeof(UpdateSensorDishPatch), "Fix"))
+                }, PatchMode.REPLACE, CheckMode.ALWAYS, false);
+        }
+
+        public static void Fix(int NetID)
+{
+    ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.SensorDishCollectScrap", PhotonTargets.MasterClient, new object[]
+    {
+                NetID,
+                PLNetworkManager.Instance.LocalPlayer.GetPawn().CurrentShip.ShipID
+    });
+}
+    }
+
+    public class PlayerShipReplace
+{
+    public static PLShipInfo PatchShip(PLSpaceScrap scrap) // Low on performance?
+    {
+        if (scrap != null)
+        {
+            List<PLShipInfo> allFleetShips = new List<PLShipInfo>();
+            foreach (PLShipInfo fleetship in PLEncounterManager.Instance.AllShips.Values)
+            {
+                if (fleetship != null && fleetship.TagID == -23)
+                {
+                   allFleetShips.Add(fleetship);
+                }
+                //Info($"NewShip: {fleetship.Key} - {((PLShipInfo)PLEncounterManager.Instance.GetShipFromID(fleetship.Value)).ShipName}");
+            }
+
+            var pos = scrap.transform.position;
+            float dist = float.MaxValue;
+            PLShipInfo selectedShip = PLEncounterManager.Instance.PlayerShip; // anti-null
+
+            foreach (var ship in allFleetShips)
+            {
+                var localdist = Vector3.Distance(ship.GetSpaceLoc(), pos);
+                //Info($"{localdist.ToString()} vs {dist} - {ship.ShipName}");
+                if (dist > localdist)
+                {
+                    dist = localdist;
+                    selectedShip = ship;
+                }
+            }
+            return selectedShip;
+        }
+
+        return PLEncounterManager.Instance.PlayerShip;
+    }
+}
 }
--- FLEETMOD/UpdateSpaceScrap.cs
+++ FLEETMOD/UpdateSpaceScrap.cs
@@ -1,24 +1,61 @@
 ﻿using System;
-using HarmonyLib;
+using PulsarModLoader;
+using UnityEngine;
 
-namespace FLEETMOD
+namespace FLEETMOD.ModMessages
 {
-	[HarmonyPatch(typeof(PLSpaceScrap), "Update")]
-	internal class UpdateSpaceScrap
-	{
-		public static void Postfix(PLSpaceScrap __instance)
-		{
-			if (MyVariables.isrunningmod)
-			{
-				foreach (PLShipInfoBase plshipInfoBase in PLEncounterManager.Instance.AllShips.Values)
-				{
-					if (PhotonNetwork.isMasterClient && plshipInfoBase != null && plshipInfoBase.ExteriorTransformCached != null && (plshipInfoBase.ExteriorTransformCached.position - __instance.transform.position).sqrMagnitude < 6400f && !__instance.Collected && plshipInfoBase.TagID < -3)
-					{
-						PLNetworkManager.Instance.LocalPlayer.FBSellAttemptsLeft = plshipInfoBase.ShipID;
-						__instance.OnCollect();
-					}
-				}
-			}
-		}
-	}
+    public class SensorDishCollectScrap : ModMessage
+    {
+        public override void HandleRPC(object[] arguments /* int NetID, int FromShip */, PhotonMessageInfo sender)
+        {
+            int NetID = (int)arguments[0];
+            PLSpaceScrap plspaceScrap = PLSpecialEncounterNetObject.GetObjectAtID(NetID) as PLSpaceScrap;
+            PLShipInfo plshipInfo = (PLShipInfo)PLEncounterManager.Instance.GetShipFromID((int)arguments[1]);
+            PLSlot slot = plshipInfo.MyStats.GetSlot(ESlotType.E_COMP_CARGO);
+            if (slot != null && (slot.Count < slot.MaxItems || plshipInfo.ShipTypeID == EShipType.E_ACADEMY))
+            {
+                plspaceScrap.Collected = true;
+                PLServer.Instance.photonView.RPC("ScrapCollectedEffect", PhotonTargets.All, new object[]
+                {
+                    plspaceScrap.transform.position
+                });
+                if (plshipInfo.ShipTypeID == EShipType.E_ACADEMY)
+                {
+                    return;
+                }
+                PLServer.Instance.photonView.RPC("ScrapLateCollected", PhotonTargets.All, new object[]
+                {
+                    NetID
+                });
+                if (plspaceScrap.IsSpecificComponentScrap)
+                {
+                    plshipInfo.MyStats.AddShipComponent(PLWare.CreateFromHash(1, plspaceScrap.SpecificComponent_CompHash) as PLShipComponent, -1, ESlotType.E_COMP_CARGO);
+                    return;
+                }
+                PLRand plrand = new PLRand(PLServer.Instance.GalaxySeed + PLServer.Instance.GetCurrentHubID() + NetID);
+                int inLevel = 0;
+                if (plspaceScrap.CanGiveComponent)
+                {
+                    int num = plrand.Next(0, 200);
+                    if (PLEncounterManager.Instance.PlayerShip.ShipTypeID == EShipType.E_CARRIER)
+                    {
+                        num = plrand.Next(0, 75);
+                    }
+                    Mathf.RoundToInt(Mathf.Pow(plrand.Next(0f, 1f), 4f) * PLServer.Instance.ChaosLevel);
+                    PLShipComponent plshipComponent = null;
+                    if (num < 50 && plspaceScrap.SpecificComponent_CompHash != -1)
+                    {
+                        plshipComponent = PLShipComponent.CreateShipComponentFromHash(plspaceScrap.SpecificComponent_CompHash, null);
+                    }
+                    if (plshipComponent == null)
+                    {
+                        plshipComponent = new PLScrapCargo(inLevel);
+                    }
+                    plshipInfo.MyStats.AddShipComponent(plshipComponent, -1, ESlotType.E_COMP_CARGO);
+                    return;
+                }
+                plshipInfo.MyStats.AddShipComponent(new PLScrapCargo(inLevel), -1, ESlotType.E_COMP_CARGO);
+            }
+        }
+    }
 }
