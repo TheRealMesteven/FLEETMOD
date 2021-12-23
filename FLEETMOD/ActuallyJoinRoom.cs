7ded0f5e3d5f191f383c5414f713a954fe967501
Steven <mestevenyoutubing@gmail.com>
2021-12-23 20:50:27 +0000

    Fixed fleetmod not initializing bug

--- FLEETMOD/ActuallyJoinRoom.cs
+++ FLEETMOD/ActuallyJoinRoom.cs
@@ -1,52 +1,75 @@
 ﻿using System;
 using HarmonyLib;
+using PulsarModLoader;
 
 namespace FLEETMOD
 {
-	[HarmonyPatch(typeof(PLUIPlayMenu), "ActuallyJoinRoom")]
-	internal class ActuallyJoinRoom
-	{
-		public static bool Prefix(RoomInfo room)
-		{
-			if ((int)room.CustomProperties["CurrentPlayersPlusBots"] < (int)room.MaxPlayers)
-			{
-				if (room.CustomProperties.ContainsKey("SteamServerID"))
-				{
-					if (!SteamManager.Initialized)
-					{
-						PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLErrorMessageMenu("Failed to join crew! Can't join Secured game when not logged in to Steam!"));
-						return false;
-					}
-					uint num = (uint)((long)room.CustomProperties["SteamServerID"]);
-					PLNetworkManager.Instance.ClearSteamAuthSession(num);
-					PLNetworkManager.Instance.SetSteamAuthTicket(num);
-				}
-				if ((string)room.CustomProperties["Ship_Type"] == Plugin.myversion)
-				{
-					MyVariables.isrunningmod = true;
-					PLNetworkManager.Instance.JoinRoom(room);
-					PLNetworkManager.Instance.StartCoroutine("ServerWaitForNetwork");
-					PLLoader.Instance.IsWaitingOnNetwork = true;
-				}
-				if (!room.CustomProperties["Ship_Type"].ToString().Contains("FLEETMOD"))
-				{
-					MyVariables.isrunningmod = false;
-					PLNetworkManager.Instance.JoinRoom(room);
-					PLNetworkManager.Instance.StartCoroutine("ServerWaitForNetwork");
-					PLLoader.Instance.IsWaitingOnNetwork = true;
-				}
-				if ((string)room.CustomProperties["Ship_Type"] != Plugin.myversion && room.CustomProperties["Ship_Type"].ToString().Contains("."))
-				{
-					MyVariables.isrunningmod = false;
-					PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLErrorMessageMenu("Sorry, This Server Is Running FleetMod Version " + (string)room.CustomProperties["Ship_Type"] + "\n You Have Version " + Plugin.myversion));
-				}
-				return false;
-			}
-			else
-			{
-				PLTabMenu.Instance.TimedErrorMsg = "Couldn't join crew! It is full!";
-				return false;
-			}
-		}
-	}
+    [HarmonyPatch(typeof(PLUIPlayMenu), "ActuallyJoinRoom")]
+    internal class ActuallyJoinRoom
+    {
+        public static bool Prefix(RoomInfo room)
+        {
+            if ((int)room.CustomProperties["CurrentPlayersPlusBots"] < (int)room.MaxPlayers)
+            {
+                if (room.CustomProperties.ContainsKey("SteamServerID"))
+                {
+                    if (!SteamManager.Initialized)
+                    {
+                        PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLErrorMessageMenu("Failed to join crew! Can't join Secured game when not logged in to Steam!"));
+                        return false;
+                    }
+                    uint num = (uint)((long)room.CustomProperties["SteamServerID"]);
+                    PLNetworkManager.Instance.ClearSteamAuthSession(num);
+                    PLNetworkManager.Instance.SetSteamAuthTicket(num);
+                }
+                if ((string)room.CustomProperties["Ship_Type"] == Plugin.myversion)
+                {
+                    PLNetworkManager.Instance.JoinRoom(room);
+                    PLNetworkManager.Instance.StartCoroutine("ServerWaitForNetwork");
+                    PLLoader.Instance.IsWaitingOnNetwork = true;
+                }
+                if (!room.CustomProperties["Ship_Type"].ToString().Contains("FLEETMOD"))
+                {
+                    PLNetworkManager.Instance.JoinRoom(room);
+                    PLNetworkManager.Instance.StartCoroutine("ServerWaitForNetwork");
+                    PLLoader.Instance.IsWaitingOnNetwork = true;
+                }
+                if ((string)room.CustomProperties["Ship_Type"] != Plugin.myversion && room.CustomProperties["Ship_Type"].ToString().Contains("."))
+                {
+                    PLNetworkManager.Instance.MainMenu.AddActiveMenu(new PLErrorMessageMenu("Sorry, This Server Is Running FleetMod Version " + (string)room.CustomProperties["Ship_Type"] + "\n You Have Version " + Plugin.myversion));
+                }
+                return false;
+            }
+            else
+            {
+                PLTabMenu.Instance.TimedErrorMsg = "Couldn't join crew! It is full!";
+                return false;
+            }
+        }
+    }
+    [HarmonyPatch(typeof(PLServer), "Start")]
+    internal class TriggerFleetmod
+    {
+        private static void Postfix()
+        {
+            if (!PhotonNetwork.isMasterClient && !MyVariables.isrunningmod)
+            {
+                ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.ActivateFleetmod", PhotonTargets.MasterClient, new object[] { });
+            }
+        }
+    }
+    public class ActivateFleetmod : ModMessage
+    {
+        public override void HandleRPC(object[] arguments, PhotonMessageInfo sender)
+        {
+            if (sender.sender == PhotonNetwork.masterClient)
+            {
+                MyVariables.isrunningmod = true;
+            }
+            else if (PhotonNetwork.isMasterClient && MyVariables.isrunningmod)
+            {
+                ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.ActivateFleetmod", sender.sender, new object[] { });
+            }
+        }
+    }
 }
