25312afbb35e86f3c7648cf62e54cd9310ce944b
Mest <41182613+TheRealMesteven@users.noreply.github.com>
2022-01-06 11:28:27 +0000

    Started Dictionaries for ShipCrews and Fleet
    Added ShipCrews, a dictionary of ShipID against PLPlayer - Will be used in future for storing players with and without the mod.
    Added Fleet, a list of ShipIDs of the Fleet.
    Added EnabledFleetmod, a list of PLPlayers who have Fleetmod enabled and running - Will be used when non modded can join additional ships

--- FLEETMOD/MyVariables.cs
+++ FLEETMOD/MyVariables.cs
@@ -12,16 +12,23 @@ namespace FLEETMOD
         public static bool recentfriendlyfire = false;
         public static bool DialogGenerated = false;
         public static bool CargoMenu = false;
+        public static Dictionary<int,PLPlayer> ShipCrews;
+        // ShipID, PLPlayer // List of crews in ship
+        public static List<int> Fleet;
+        // ShipIDs of the ships in the Fleet
+        public static List<PLPlayer> EnabledFleetmod;
+        // PLPlayer of the Players who have Fleetmod active and running
+        
         public static int GetShipCaptain (int inShipID)
         {
-            foreach (PLPlayer pLPlayer in PLServer.Instance.AllPlayers)
+            foreach (PLPlayer pLPlayer in ShipCrews[inShipID])
             {
                 if (pLPlayer != null && pLPlayer.GetClassID() == 0 && pLPlayer.TeamID == 0)
                 {
-                    if (pLPlayer.GetPhotonPlayer().GetScore() == inShipID)
-                    {
+                    //if (pLPlayer.GetPhotonPlayer().GetScore() == inShipID)
+                    //{
                         return pLPlayer.GetPlayerID();
-                    }
+                    //}
                 }
             }
             return -1;
