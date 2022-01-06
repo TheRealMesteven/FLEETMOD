f8f73421f78a84460ec52d647cd777ca47d3dd80
Mest <41182613+TheRealMesteven@users.noreply.github.com>
2022-01-06 11:36:34 +0000

    Added initial Dictionary and List creation

--- FLEETMOD/ServerUpdateVariables.cs
+++ FLEETMOD/ServerUpdateVariables.cs
@@ -13,4 +13,31 @@ namespace FLEETMOD
             //MyVariables.warprange = (float)arguments[1];
         }
     }
+    [HarmonyPatch(typeof(PLServer), "LoginMessage")] //Initial Sync. If multiple messages exist consider making new Modmessage for initial sync
+    class LoginMessagePatch
+    {
+        static void Postfix(PhotonPlayer newPhotonPlayer)
+        {
+            if (PhotonNetwork.isMasterClient)
+            {
+                /*
+                PulsarPluginLoader.ModMessage.SendRPC("Dragon+Mest.Fleetmod", "FLEETMOD.ModMessages.SyncCrewIDs", newPhotonPlayer, new object[] {
+                    Global.Fleet,
+                    Global.PlayerCrewList
+                });
+                */
+            }
+        }
+    }
+    [HarmonyPatch(typeof(PLServer), "Start")]
+    class StartPatch
+    { /// Initial Patch creating the dictionaries and lists.
+        static void Postfix()
+        {
+            MyVariables.Fleet = new List<int>();
+            MyVariables.EnabledFleetmod = new List<PLPlayer>();
+            MyVariables.ShipCrews = new Dictionary<int, PLPlayer>();
+            EnabledFleetmod.Add(PLNetworkManager.Instance.LocalPlayer);
+        }
+    }
 }
